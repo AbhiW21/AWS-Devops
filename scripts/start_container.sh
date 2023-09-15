10684950fdadeaa5344e6a67a92c5b29a4131909
AbhiW21 <122991139+AbhiW21@users.noreply.github.com>
2023-09-15 15:30:35 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Pull docker image from docker hub
-docker pull abhiw/simple-python-flask-app:latest
+docker pull abhiw/simple-python-flask-app
 
 #Run the docker image on a container
-docker run -d -p 5000:5000 abhiw/simple-python-flask-app:latest
+docker run -d -p 5000:5000 abhiw/simple-python-flask-app
