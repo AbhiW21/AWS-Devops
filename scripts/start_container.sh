8027037cbdcc0b6e6d479477d4166f20cca2607e
AbhiW21 <122991139+AbhiW21@users.noreply.github.com>
2025-11-17 18:03:06 +0530

    Delete scripts/start_container.sh

--- scripts/start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-#Pull docker image from docker hub
-docker pull abhiw/simple-python-flask-app
-
-#Run the docker image on a container
-docker run -d -p 5000:5000 abhiw/simple-python-flask-app
-
